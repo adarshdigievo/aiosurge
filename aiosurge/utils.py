1b819b0d2abedb5fd7d4e9fcd73f6e1dc206557e
Adarsh D <adarshdevamritham@gmail.com>
2025-04-20 18:26:20 +0530

    feat: convert utils.py to async

---
+++ aiosurge/utils.py
@@ -0,0 +1,19 @@
+from aiocsv import AsyncReader
+import aiofiles
+
+async def load_tasks_data_from_csv(file_path: str):
+    tasks_data = []
+
+    async with aiofiles.open(file_path) as csvfile:
+        reader = AsyncReader(csvfile)
+
+        headers = await anext(reader, None)
+        assert type(headers) is list and len(headers) > 0
+
+        async for row in reader:
+            data = {}
+            for i in range(len(headers)):
+                data[headers[i]] = row[i]
+            tasks_data.append(data)
+
+    return tasks_data
